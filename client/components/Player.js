5a19468899f62a503155d1b909598a97e3bbb370
Dral <m.c.dral@gmail.com>
2015-12-24 16:57:42 +0100

    Add input field to control volume

--- client/components/Player.js
+++ client/components/Player.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {compose, withState} from 'recompose'
 
 import {clickable} from '../style.css'
-import {Audio, View, Text} from '../components'
+import {Audio, View, Text, TextInput} from '../components'
 
 let Player = compose(
   withState('volume', 'setVolume', 0.1)
@@ -10,13 +10,25 @@ let Player = compose(
   <View>
     <Audio volume={volume} src={`${URL}${time}.mp3`} autoPlay />
 
+    <TextInput
+      style={{width: 30}}
+      onTextChange={text => {
+        let num = parseInt(text)
+        if (!Number.isNaN(num)) {
+          setVolume(num / 100)
+        } else {
+          setVolume(0)
+        }
+      }}
+      value={Math.ceil(volume * 100)}
+    />%
+
     <View>
-      {volume}
       <View className={clickable} onClick={() => setVolume(Math.min(volume + 0.1, 1))}>
-        HARDER { volume === 1 && '(Nog harder heeft geen zin srry)'}
+        HARDER { volume >= 1 && '(Nog harder heeft geen zin srry)'}
       </View>
       <View className={clickable} onClick={() => setVolume(Math.max(volume - 0.1, 0))}>
-        ZACHTER { volume === 0 && '(Hij is al op z\'n zachts)'}
+        ZACHTER { volume <= 0 && '(Hij is al op z\'n zachts)'}
       </View>
     </View>
   </View>
